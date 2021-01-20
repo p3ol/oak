64d21a1adba7141cab9c25925fafb15d75b8023b
Maxime Da Silva <maximed@poool.fr>
2021-01-20 14:40:40 +0100

    feat(rows): add style for lateral division, arrows and allow puhing element on top and bottom

--- packages/oak/lib/core/Row/index.js
+++ packages/oak/lib/core/Row/index.js
@@ -14,19 +14,52 @@ const Row = ({ className, element }) => {
 
   useLayoutEffect(() => {
     if (!element.cols?.length) {
-      setElement(element, { cols: [{ size: 12, content: [] }] });
+      setElement(element, { cols: [{ size: 6, content: [], id: 0 },
+        { size: 6, content: [], id: 1 }] });
+
     }
   }, []);
 
-  const onAppend = (col, component) => {
-    addElement(component.construct(), col.content);
+  const onAppend = (col, isFirst, component) => {
+    addElement(component.construct(), col.content, isFirst);
     catalogueRef.current?.close();
+  };
+
+  const divide = (currentCol, isBefore) => {
+    const index = element.cols.indexOf(currentCol);
+    const idMax = element.cols.map(col => col.id).reduce(function (a, b) {
+      return Math.max(a, b);
+    }) + 1;
+    element.cols.splice(isBefore ? index : index + 1, 0,
+      { size: 6, content: [], id: idMax }
+    );
+    setElement(element, { cols: element.cols });
+  };
+
+  const remove = (element, col) => {
+    setElement(element, { cols: element.cols.filter(c => c.id !== col.id) },
+    );
   };
 
   return (
     <div className={classNames(className, styles.row)}>
       { element?.cols?.map((col, i) => (
         <div className={styles.col} key={i}>
+          <a href="#" onClick={remove.bind(null, element, col)}>
+            <span className="material-icons">
+             delete
+            </span>
+          </a>
+          { col.content.length > 0 &&
+              <div className={styles.addElement}>
+                <Catalogue
+                  className={styles.catalogue}
+                  ref={catalogueRef}
+                  onAppend={onAppend.bind(null, col, true)}
+                />
+              </div>
+          }
+
           <div className={styles.content}>
             { col.content?.map((item, i) => (
               <Element
@@ -37,13 +70,23 @@ const Row = ({ className, element }) => {
               />
             )) }
           </div>
+          <div className={styles.flex}>
+            <a href="#" onClick={divide.bind(null, col, true)}>
+              <span className="material-icons">
+                chevron_left
+              </span></a>
+            <div className={styles.addElement}>
+              <Catalogue
+                className={styles.catalogue}
+                ref={catalogueRef}
+                onAppend={onAppend.bind(null, col, false)}
+              />
+            </div>
 
-          <div className={styles.addElement}>
-            <Catalogue
-              className={styles.catalogue}
-              ref={catalogueRef}
-              onAppend={onAppend.bind(null, col)}
-            />
+            <a href="#" onClick={divide.bind(null, col, false)}>
+              <span className="material-icons">
+                chevron_right
+              </span></a>
           </div>
         </div>
       ))}
