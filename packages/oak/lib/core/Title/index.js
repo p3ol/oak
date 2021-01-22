b79ea02bf8e47aa8ef1e439b79091f55956c8ce5
Maxime Da Silva <maximed@poool.fr>
2021-01-22 09:50:54 +0100

    feat(page builder): add title component

---
+++ packages/oak/lib/core/Title/index.js
@@ -0,0 +1,24 @@
+import React, { useRef, useContext, useLayoutEffect } from 'react';
+
+import { AppContext } from '../../contexts';
+
+import styles from '../Text/index.styl';
+
+const Title = ({ className, element }) => {
+  const catalogueRef = useRef();
+  const { setElement } = useContext(AppContext);
+
+  return (
+    element.editing
+      ? <textarea id="story" name="story" className={styles.textarea}
+        rows="5" onChange={value => setElement(element, { content: value.target.value })}>
+        {element.content}
+      </textarea>
+      : (element.headingLevel === 1 ? <h1>{element.content}</h1>
+        : element.headingLevel === 2 ? <h2>{element.content}</h2>
+          : element.headingLevel === 3 ? <h3>{element.content}</h3>
+            : <h4>{element.content}</h4>)
+  );
+};
+
+export default Title;
