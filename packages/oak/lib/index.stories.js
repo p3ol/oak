699895e63f537714f0c25175b3ee26c922538151
Maxime Da Silva <maximed@poool.fr>
2021-02-08 17:24:57 +0100

    fix(builder): lint on input json

--- packages/oak/lib/index.stories.js
+++ packages/oak/lib/index.stories.js
@@ -19,7 +19,22 @@ export const basicConfig = () => {
               content: [
                 {
                   type: 'text',
-                  content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+                  content: '1Sed ut perspiciatis unde omnis iste natus error' +
+                  ' sit voluptatem accusantium doloremque laudantium, totam' +
+                  ' rem aperiam, eaque ipsa quae ab illo inventore veritatis' +
+                  ' et quasi architecto beatae vitae dicta sunt explicabo. ' +
+                  'Nemo enim ipsam voluptatem quia voluptas sit aspernatur ' +
+                  'aut odit aut fugit, sed quia consequuntur magni dolores ' +
+                  'eos qui ratione voluptatem sequi nesciunt. Neque porro ' +
+                  'quisquam est, qui dolorem ipsum quia dolor sit amet,' +
+                  ' consectetur, adipisci velit, sed quia non numquam eius' +
+                  ' modi tempora incidunt ut labore et dolore magnam aliquam ' +
+                  'quaerat voluptatem. Ut enim ad minima veniam, quis nostrum' +
+                  'exercitationem ullam corporis suscipit laboriosam, nisi ' +
+                  'ut aliquid ex ea commodi consequatur? Quis autem vel eum ' +
+                  'iure reprehenderit qui in ea voluptate velit esse quam ' +
+                  'nihil molestiae consequatur, vel illum qui dolorem eum ' +
+                  'fugiat quo voluptas nulla pariatur?',
                   style: {
                     horizontalAlignement: 'center',
                   },
@@ -43,7 +58,22 @@ export const basicConfig = () => {
                       content: [
                         {
                           type: 'text',
-                          content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+                          content: '1Sed ut perspiciatis unde omnis iste ' +
+                          ' sit voluptatem accusantium doloremque laudant,' +
+                          ' rem aperiam, eaque ipsa quae ab illo inventor' +
+                          ' et quasi architecto beatae vitae dicta sunt. ' +
+                          'Nemo enim ipsam voluptatem quia aspernatur ' +
+                          'aut odit aut fugit, sed quia magni dolores ' +
+                          'eos qui ratione voluptatem sequi. Neque porro ' +
+                          'quisquam est, qui dolorem ipsum dolor sit amet,' +
+                          ' consectetur, adipisci velit, sed non numquam eius' +
+                          ' modi tempora incidunt ut labore magnam aliquam ' +
+                          'quaerat voluptatem. Ut enim ad , quis nostrum' +
+                          'exercitationem ullam corporis suscipit , nisi ' +
+                          'ut aliquid ex commodi consequatur? Quis vel eum ' +
+                          'iure reprehenderit qui in ea voluptate velit quam ' +
+                          'nihil molestiae consequatur, vel illum qui eum ' +
+                          'fugiat quo voluptas nulla pariatur?',
                         },
                       ],
                       id: 0,
