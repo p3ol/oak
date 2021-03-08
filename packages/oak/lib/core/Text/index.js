e30fe8a62aa71540c5031d9f6770d38e19e98739
Maxime Da Silva <maximed@poool.fr>
2021-03-08 11:11:37 +0100

    feat(popper): add pedit popper on every component, for testing purposes

--- packages/oak/lib/core/Text/index.js
+++ packages/oak/lib/core/Text/index.js
@@ -1,6 +1,7 @@
 import React, { useRef, useContext, useLayoutEffect } from 'react';
 
 import { AppContext } from '../../contexts';
+import Edit from '../Edit';
 import Option from '../Option';
 
 import styles from './index.styl';
@@ -30,13 +31,7 @@ Text.options = [{
     const { setElement } = useContext(AppContext);
 
     return (
-      <Option
-        option={{ icon: 'edit' }}
-        onClick={() => {
-
-          setElement(element, { editing: !element.editing });
-        }}
-      />
+      <Edit></Edit>
     );
   },
 }];
