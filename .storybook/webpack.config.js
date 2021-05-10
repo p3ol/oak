3fc2a41e83310437cf33345c7d387988f6fd49ec
Maxime Da Silva <maximed@poool.fr>
2021-05-10 17:31:20 +0200

    chore: add relative path dependancy to Aspen

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
-
 module.exports = ({ config }) => {
   config.resolve.modules = ['node_modules', path.resolve('./node_modules')];
   config.resolve.alias = {
     'react': 'preact/compat',
     'react-dom': 'preact/compat',
+    '@poool/aspen': path.resolve('../aspen/src')
   };
-
   config.module.rules.push({
     test: /\.styl$/,
     use: [
