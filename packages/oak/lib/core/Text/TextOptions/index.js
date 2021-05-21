aa78aa791fd7d1afb9719da81a98b341dc2baa69
Maxime Da Silva <maximed@poool.fr>
2021-05-21 12:35:26 +0200

    feat(oak): move text options

---
+++ packages/oak/lib/core/Text/TextOptions/index.js
@@ -0,0 +1,88 @@
+import { ColorPicker, SelectField, Tab, Tabs, TextField } from '@poool/junipero';
+import React, { useContext } from 'react';
+
+import { AppContext } from '../../../contexts';
+import TextEditor from '../../TextEditor';
+
+import styles from '../index.styl'
+;
+
+const TextOptions = ({ element }) => {
+  const { setElement } = useContext(AppContext);
+  const horizontal = [
+    { title: 'Aligné à gauche', value: 'flex-start' },
+    { title: 'Centré', value: 'center' },
+    { title: 'Aligné à droite', value: 'flex-end' },
+  ];
+
+  const textAlign = [
+    { title: 'Aligné à gauche', value: 'start' },
+    { title: 'Centré', value: 'center' },
+    { title: 'Aligné à droite', value: 'end' },
+    { title: 'Justifié', value: 'justify' },
+  ];
+
+  return (
+    <Tabs>
+      <Tab title="Settings">
+        <div id="wysiwyg">
+          <TextEditor
+            onChange={item => {
+              setElement(element, { content: item.value });
+            }}
+            value={element.content}
+          />
+        </div>
+      </Tab>
+      <Tab title="Style">
+        <div className={styles.flex}>
+          <ColorPicker
+            value={element.style?.color || '#000000'}
+            placeholder="Couleur du texte"
+            label="Couleur du texte"
+            onChange={item => {
+              setElement(element.style, { color: item.value });
+            }}
+          />
+          <SelectField
+            label="Alignement horizontal"
+            placeholder="Alignement horizontal"
+            boxed={false}
+            value={element.style?.horizontalAlignement || 'flex-start'}
+            parseValue={item => item.value}
+            parseTitle={item => item.title}
+            className={styles.item}
+            onChange={item => {
+              setElement(element.style, { horizontalAlignement: item.value });
+            }}
+            options={horizontal}
+          />
+          <SelectField
+            label="Alignement texte"
+            placeholder="Alignement texte"
+            boxed={false}
+            value={element.style?.textAlign || 'start'}
+            parseValue={item => item.value}
+            parseTitle={item => item.title}
+            className={styles.item}
+            onChange={item => {
+              setElement(element.style, { textAlign: item.value });
+            }}
+            options={textAlign}
+          />
+          <TextField
+            boxed={false}
+            placeholder="Largeur fixe"
+            label="Largeur fixe"
+            value={element.style?.width}
+            onChange={item => {
+              setElement(element.style, { width: item.value });
+            }}
+          />
+        </div>
+      </Tab>
+    </Tabs>
+  );
+};
+
+export default TextOptions;
--- packages/oak/lib/core/Text/index.js
+++ packages/oak/lib/core/Text/index.js
@@ -1,11 +1,9 @@
-import { ColorPicker, SelectField, TextField } from '@poool/junipero';
+import { Tab, Tabs } from '@poool/junipero';
 import React, { useContext } from 'react';
 
 import { AppContext } from '../../contexts';
 import EditBox from '../EditBox';
-import TextEditor from '../TextEditor';
-
-import styles from './index.styl';
+import TextOptions from './TextOptions';
 
 const Text = ({ element }) => {
 
@@ -20,72 +18,9 @@ const Text = ({ element }) => {
 
 Text.options = [{
   render: ({ element }) => {
-    const { setElement } = useContext(AppContext);
-    const horizontal = [
-      { title: 'Aligné à gauche', value: 'flex-start' },
-      { title: 'Centré', value: 'center' },
-      { title: 'Aligné à droite', value: 'flex-end' },
-    ];
-
-    const textAlign = [
-      { title: 'Aligné à gauche', value: 'start' },
-      { title: 'Centré', value: 'center' },
-      { title: 'Aligné à droite', value: 'end' },
-      { title: 'Justifié', value: 'justify' },
-    ];
-
     return (
-      <EditBox title='Text options'>
-        <ColorPicker
-          value={element.style?.color || '#000000'}
-          placeholder='Couleur du texte'
-          label='Couleur du texte'
-          onChange={item => {
-            setElement(element.style, { color: item.value });
-          }}
-        />
-        <SelectField
-          label="Alignement horizontal"
-          placeholder="Alignement horizontal"
-          boxed={false}
-          value={element.style?.horizontalAlignement || 'flex-start'}
-          parseValue={item => item.value}
-          parseTitle={item => item.title}
-          className={styles.item}
-          onChange={item => {
-            setElement(element.style, { horizontalAlignement: item.value });
-          }}
-          options={horizontal}
-        />
-        <SelectField
-          label="Alignement texte"
-          placeholder="Alignement texte"
-          boxed={false}
-          value={element.style?.textAlign || 'start'}
-          parseValue={item => item.value}
-          parseTitle={item => item.title}
-          className={styles.item}
-          onChange={item => {
-            setElement(element.style, { textAlign: item.value });
-          }}
-          options={textAlign}
-        />
-        <TextField
-          boxed={false}
-          placeholder="Largeur fixe"
-          label="Largeur fixe"
-          value={element.style?.width}
-          onChange={item => {
-            setElement(element.style, { width: item.value });
-          }}
-        />
-        <div id="wysiwyg">
-          <TextEditor
-            onChange={item => {
-              setElement(element, { content: item.value });
-            }}
-            value={element.content} />
-        </div>
+      <EditBox title="Text options">
+        <TextOptions element={element} />
       </EditBox>
     );
   },
