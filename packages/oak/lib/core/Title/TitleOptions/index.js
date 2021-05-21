f83631db2e62c0934ffe3667ed0fc538a1a8e7e6
Maxime Da Silva <maximed@poool.fr>
2021-05-21 12:35:38 +0200

    feat(oak): move title options

---
+++ packages/oak/lib/core/Title/TitleOptions/index.js
@@ -0,0 +1,110 @@
+import {
+  ColorPicker,
+  SelectField,
+  Tab,
+  Tabs,
+  TextField }
+  from '@poool/junipero';
+import React, { useContext } from 'react';
+
+import { AppContext } from '../../../contexts';
+import TextEditor from '../../TextEditor';
+
+import styles from '../../Text/index.styl';
+
+const TitleOptions = ({ element }) => {
+  const { setElement } = useContext(AppContext);
+  const headings = [
+    { title: 'Heading 1', value: 'h1' },
+    { title: 'Heading 2', value: 'h2' },
+    { title: 'Heading 3', value: 'h3' },
+    { title: 'Heading 4', value: 'h4' },
+    { title: 'Heading 5', value: 'h5' },
+    { title: 'Heading 6', value: 'h6' },
+  ];
+  const horizontal = [
+    { title: 'Aligné à gauche', value: 'flex-start' },
+    { title: 'Centré', value: 'center' },
+    { title: 'Aligné à droite', value: 'flex-end' },
+  ];
+
+  const textAlign = [
+    { title: 'Aligné à gauche', value: 'start' },
+    { title: 'Centré', value: 'center' },
+    { title: 'Aligné à droite', value: 'end' },
+    { title: 'Justifié', value: 'justify' },
+  ];
+
+  return (
+    <Tabs>
+      <Tab title="Settings">
+        <div>
+          <SelectField
+            label="Alignement horizontal"
+            boxed={false}
+            value={element.headingLevel || 'h1'}
+            parseValue={item => item.value}
+            parseTitle={item => item.title}
+            className={styles.item}
+            onChange={item => {
+              setElement(element, { headingLevel: item.value });
+            }}
+            options={headings}
+          />
+          <div id="wysiwyg">
+            <TextEditor
+              onChange={item => {
+                setElement(element, { content: item.value });
+              }}
+              value={element.content}
+            />
+          </div>
+        </div>
+      </Tab>
+      <Tab title="Style">
+        <div className={styles.flex}>
+          <ColorPicker
+            value={element.style?.color || '#000000'}
+            onChange={item => {
+              setElement(element.style, { color: item.value });
+            }}
+          />
+          <SelectField
+            label="Alignement horizontal"
+            boxed={false}
+            value={element.style?.horizontalAlignement || 'flex-start'}
+            parseValue={item => item.value}
+            parseTitle={item => item.title}
+            className={styles.item}
+            onChange={item => {
+              setElement(element.style, { horizontalAlignement: item.value });
+            }}
+            options={horizontal}
+          />
+          <SelectField
+            label="Alignement texte"
+            boxed={false}
+            value={element.style?.textAlign || 'start'}
+            parseValue={item => item.value}
+            parseTitle={item => item.title}
+            className={styles.item}
+            onChange={item => {
+              setElement(element.style, { textAlign: item.value });
+            }}
+            options={textAlign}
+          />
+          <TextField
+            boxed={false}
+            placeholder="Largeur fixe"
+            value={element.style?.width}
+            onChange={item => {
+              setElement(element.style, { width: item.value });
+            }}
+          />
+        </div>
+      </Tab>
+    </Tabs>
+  );
+};
+
+export default TitleOptions;
--- packages/oak/lib/core/Title/index.js
+++ packages/oak/lib/core/Title/index.js
@@ -4,6 +4,7 @@ import React, { useContext } from 'react';
 import { AppContext } from '../../contexts';
 import EditBox from '../EditBox';
 import TextEditor from '../TextEditor';
+import TitleOptions from './TitleOptions';
 
 import styles from '../Text/index.styl';
 
@@ -14,7 +15,8 @@ const Title = ({ element }) => {
       color: element.style?.color,
       textAlign: element.style?.textAlign,
       width: element.style?.width,
-    }}>
+    }}
+    >
       {React.createElement(element.headingLevel || 'h1', {
         className: styles.title,
         dangerouslySetInnerHTML: { __html: element.content },
@@ -26,87 +28,10 @@ const Title = ({ element }) => {
 
 Title.options = [{
   render: ({ element }) => {
-    const { setElement } = useContext(AppContext);
-    const headings = [
-      { title: 'Heading 1', value: 'h1' },
-      { title: 'Heading 2', value: 'h2' },
-      { title: 'Heading 3', value: 'h3' },
-      { title: 'Heading 4', value: 'h4' },
-      { title: 'Heading 5', value: 'h5' },
-      { title: 'Heading 6', value: 'h6' },
-    ];
-    const horizontal = [
-      { title: 'Aligné à gauche', value: 'flex-start' },
-      { title: 'Centré', value: 'center' },
-      { title: 'Aligné à droite', value: 'flex-end' },
-    ];
-
-    const textAlign = [
-      { title: 'Aligné à gauche', value: 'start' },
-      { title: 'Centré', value: 'center' },
-      { title: 'Aligné à droite', value: 'end' },
-      { title: 'Justifié', value: 'justify' },
-    ];
-
     return (
-      <EditBox title='Title options'>
-        <SelectField
-          label="Alignement horizontal"
-          boxed={false}
-          value={element.headingLevel || 'h1'}
-          parseValue={item => item.value}
-          parseTitle={item => item.title}
-          className={styles.item}
-          onChange={item => {
-            setElement(element, { headingLevel: item.value });
-          }}
-          options={headings}
-        />
-        <ColorPicker
-          value={element.style?.color || '#000000'}
-          onChange={item => {
-            setElement(element.style, { color: item.value });
-          }}
-        />
-        <SelectField
-          label="Alignement horizontal"
-          boxed={false}
-          value={element.style?.horizontalAlignement || 'flex-start'}
-          parseValue={item => item.value}
-          parseTitle={item => item.title}
-          className={styles.item}
-          onChange={item => {
-            setElement(element.style, { horizontalAlignement: item.value });
-          }}
-          options={horizontal}
-        />
-        <SelectField
-          label="Alignement texte"
-          boxed={false}
-          value={element.style?.textAlign || 'start'}
-          parseValue={item => item.value}
-          parseTitle={item => item.title}
-          className={styles.item}
-          onChange={item => {
-            setElement(element.style, { textAlign: item.value });
-          }}
-          options={textAlign}
-        />
-        <TextField
-          boxed={false}
-          placeholder="Largeur fixe"
-          value={element.style?.width}
-          onChange={item => {
-            setElement(element.style, { width: item.value });
-          }}
-        />
-        <div id="wysiwyg">
-          <TextEditor
-            onChange={item => {
-              setElement(element, { content: item.value });
-            }}
-            value={element.content} />
-        </div>
+      <EditBox title="Title options">
+        <TitleOptions element={element} />
+
       </EditBox>
     );
   },
