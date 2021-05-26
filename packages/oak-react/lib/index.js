9b9ed651e0687c03bea30a06e26e3169c870f337
dackmin <ugo@poool.fr>
2021-05-26 15:10:12 +0200

    fix: fix import

--- packages/oak-react/lib/index.js
+++ packages/oak-react/lib/index.js
@@ -1,5 +1,5 @@
 import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
-import { render } from '@poool/oak/dist/react/esm/index.js';
+import { render } from '@poool/oak/dist/esm/index.js';
 
 export const Builder = forwardRef(({ options, ...rest }, ref) => {
   const innerRef = useRef();
