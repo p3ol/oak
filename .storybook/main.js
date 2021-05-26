d42901efaf0560de656cd21b23ad3ddf78cf4ed4
dackmin <ugo@poool.fr>
2021-05-26 16:08:00 +0200

    chore: move webpack config to storybook main file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,37 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  stories: ['../packages/*/lib/**/*.stories.js'],
+  stories: [
+    '../packages/*/lib/**/*.stories.js',
+  ],
   core: {
     builder: 'webpack5',
-  }
+  },
+  webpackFinal: config => {
+    config.resolve.alias = {
+      '@poool/oak/lib': path.resolve('./packages/oak/lib'),
+      '@poool/oak': path.resolve('./packages/oak/lib'),
+    };
+
+    config.module.rules.push({
+      test: /\.styl$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              sourceMap: true,
+              plugins: [autoprefixer],
+            },
+          },
+        },
+        'stylus-loader',
+      ],
+    });
+
+    return config;
+  },
 };
