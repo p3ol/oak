f1309c6efcca951089c0da5f539e6c8ba383fcb7
Maxime Da Silva <maximed@poool.fr>
2021-11-17 12:05:14 +0100

    feat(prosemirror): add getMarkAttrs function to know which attribute is currently active

---
+++ packages/oak-prosemirror-editor/lib/core/Editor/utils.js
@@ -0,0 +1,12 @@
+export const getMarkAttrs = (editorState, type) => {
+  const { from, to } = editorState.selection;
+  let marks = [];
+
+  editorState.doc.nodesBetween(from, to, node => {
+    marks = [...marks, ...node.marks];
+  });
+
+  const mark = marks.find(markItem => markItem.type.name === type.name);
+
+  return mark ? mark.attrs : {};
+};
