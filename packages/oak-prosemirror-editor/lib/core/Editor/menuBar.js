023a970462c1646e8cbb135c5315ad4178fdeafc
Maxime Da Silva <maximed@poool.fr>
2021-11-17 12:04:17 +0100

    feat(prosemirror): wip first POC of size on content

--- packages/oak-prosemirror-editor/lib/core/Editor/menuBar.js
+++ packages/oak-prosemirror-editor/lib/core/Editor/menuBar.js
@@ -15,6 +15,9 @@ export default ({ onToggleMark, onToggleBlock }) => {
       <button onClick={onToggleMark.bind(null, schema.marks.underline)}>
         underline
       </button>
+      <button onClick={onToggleMark.bind(null, schema.marks.size, { size: '18px' })}>
+        +
+      </button>
       <ColorButton
         onChange={color => onToggleMark(schema.marks.color, { color })}
       />
