eff888f71302e0d3c88a1acd26666183b2cc9dc5
Maxime Da Silva <maximed@poool.fr>
2021-11-26 16:46:07 +0100

    feat(prosemirror): add stories

---
+++ packages/oak-prosemirror-editor/lib/index.stories.js
@@ -0,0 +1,50 @@
+import { SelectField } from '@poool/junipero';
+import { useState } from 'react';
+
+import Editor from './core/Editor';
+
+export default { title: 'oak-prosemirror-editor' };
+
+export const classicEditor = () => {
+  const [value, setValue] = useState('This is a <strong>fancy</strong> text<br />with a line break');
+
+  const onChange = ({ value }) => {
+    setValue(value);
+  };
+
+  return (
+    <Editor
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export const titleEditor = () => {
+  const [value, setValue] = useState('This is a <strong>fancy</strong> text<br />with a line break');
+  const [headingLevel, setHeadingLevel] = useState('h1');
+  const options = Array.from(
+    { length: 6 },
+    (v, k) => ({ value: `h${k + 1}`, title: `heading ${k + 1}` })
+  );
+
+  const onChange = ({ value }) => {
+    setValue(value);
+  };
+
+  return (
+    <>
+      <SelectField
+        options={options}
+        placeholder="Choose one item"
+        parseTitle={v => v.title}
+        onChange={({ value }) => setHeadingLevel(value.value)}
+      />
+      <Editor
+        value={value}
+        onChange={onChange}
+        element={{ type: 'title', headingLevel }}
+      />
+    </>
+  );
+};
