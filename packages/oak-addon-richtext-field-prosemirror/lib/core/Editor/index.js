ab349ce503295b6e0f45634ba228cf63f0727d79
Maxime Da Silva <maximed@poool.fr>
2022-11-25 10:20:14 +0100

    fix(prosemirror): remove unused function argument

--- packages/oak-addon-richtext-field-prosemirror/lib/core/Editor/index.js
+++ packages/oak-addon-richtext-field-prosemirror/lib/core/Editor/index.js
@@ -66,7 +66,7 @@ export default ({ value, onChange, element, options = {} }) => {
     const { empty } = state.selection;
 
     if (empty) {
-      updateActiveLink(attr, viewRef)(state, tr => onChange_(state.apply(tr)));
+      updateActiveLink(attr)(state, tr => onChange_(state.apply(tr)));
     } else {
       removeActiveMark(schema.marks.link)(state, tr => {
         const transitionState = state.apply(tr);
