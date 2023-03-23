754d6f20fcd186df5f53b8f48de91d2c8f669b48
Ugo Stephant <ugo@poool.fr>
2023-03-23 18:26:44 +0100

    chore: add stories content

--- packages/react/lib/index.stories.js
+++ packages/react/lib/index.stories.js
@@ -4,8 +4,15 @@ import { baseAddon } from './addons';
 export default { title: 'React/Builder' };
 
 const baseContent = [
-  { type: 'row', content: [] },
-  { type: 'text', content: 'Test' },
+  { type: 'row', cols: [
+    { content: [
+      { type: 'title', content: 'This is a title' },
+      { type: 'text', content: 'This is a text' },
+      { type: 'empty-space', settings: { height: '20px' } },
+      { type: 'button', content: 'Click me' },
+    ] },
+  ] },
+  { type: 'image', url: 'https://avatars.githubusercontent.com/u/20414672' },
 ];
 
 export const basic = () => (
