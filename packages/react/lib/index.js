efc77eed8b58b23a39daba33e811443ee279466e
Ugo Stephant <ugo@poool.fr>
2023-03-24 17:31:36 +0100

    fix: add needed exports

--- packages/react/lib/index.js
+++ packages/react/lib/index.js
@@ -1,3 +1,13 @@
+export {
+  classNames,
+} from '@junipero/react';
+
 export { default as Builder } from './Builder';
+export { default as Icon } from './Icon';
+export { default as Text } from './Text';
+
+export {
+  useBuilder,
+} from './hooks';
 
 export * from './addons';
