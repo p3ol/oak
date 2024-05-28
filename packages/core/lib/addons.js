1d797269cc69e0c2b68fce4fb39dbfdf8a711349
maxime Da Silva <maximed@poool.fr>
2024-05-28 14:19:14 +0200

    fix(floating-ui): add shift option to floating col and row options

--- packages/core/lib/addons.js
+++ packages/core/lib/addons.js
@@ -57,12 +57,8 @@ export const rowComponent = (...props) => ({
     title: t => t('core.components.row.settings.title', 'Row options'),
     floatingSettings: {
       placement: 'right-start',
-      shift: {
-        enabled: false,
-      },
-      autoPlacement: {
-        alignment: 'start',
-      },
+      shift: { enabled: true },
+      autoPlacement: { alignment: 'start' },
     },
     fields: [{
       type: 'select',
@@ -222,10 +218,10 @@ export const colComponent = (...props) => ({
   editable: true,
   usable: false,
   settings: {
-    title: t => t('core.components.col.settings.title', 'Col options'),
+    title: t => t('core.components.col.settings.titssle', 'Colsdqdsqs options'),
     floatingSettings: {
       placement: 'left-start',
-      shift: { enabled: false },
+      shift: { enabled: true },
       autoPlacement: { enabled: false },
     },
     fields: [{
