4e27d9eacd282ab1e61d5c20184cfa88019d4b7e
Ugo Stephant <ugo@poool.fr>
2024-06-21 11:45:49 +0200

    fix(react): correctly apply settings overrides

--- packages/react/lib/utils.ts
+++ packages/react/lib/utils.ts
@@ -24,3 +24,23 @@ export const sanitizeHTML = (content: string) => {
     return '';
   }
 };
+
+export function assignDefined<T extends any> (
+  target: T,
+  ...sources: Array<any>
+): T {
+  for (const source of sources) {
+    if (!source) {
+      continue;
+    }
+
+    for (const key in source) {
+      if (typeof source[key] !== 'undefined') {
+        // @ts-ignore it's obviously any
+        target[key] = source[key];
+      }
+    }
+  }
+
+  return target;
+}
