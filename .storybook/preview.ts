40fa3e71377fb0210bd6ca2397f8b9fbe1ff53c4
Ugo Stephant <ugo@poool.fr>
2024-07-08 09:20:17 +0200

    chore: fix missing preview theme toggle

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,1 +1,18 @@
+import type { Preview } from '@storybook/react';
+import { withThemeByClassName } from '@storybook/addon-themes';
+
 import './index.sass';
+
+const preview: Preview = {
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+};
+
+export default preview;
