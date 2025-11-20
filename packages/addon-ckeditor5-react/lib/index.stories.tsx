6ecd50ef26fa1c6f098828742d752d96bcd70079
maxime Da Silva <maximed@poool.fr>
2025-11-20 14:29:20 +0100

    feat(field-override): add example on story

--- packages/addon-ckeditor5-react/lib/index.stories.tsx
+++ packages/addon-ckeditor5-react/lib/index.stories.tsx
@@ -5,10 +5,12 @@ import {
   Builder,
   baseAddon,
 } from '@oakjs/react';
+import { Field, FieldOverrideObject } from 'packages/core/dist/types';
 
 import { ckeditorFieldAddon } from './addons';
 
 export default { title: 'React/With addon: CKEditor' };
+import './index.stories.sass';
 
 const baseContent: ElementObject[] = [
   { type: 'text', content: 'This is a title' },
@@ -22,9 +24,8 @@ const addon: AddonObject = {
       key: 'content',
       type: 'ckeditor',
     }],
-  }],
+  } ],
 };
-
 export const Basic = () => (
   <Builder
     addons={[baseAddon(), ckeditorFieldAddon({
@@ -38,3 +39,42 @@ export const Basic = () => (
     onChange={action('change')}
   />
 );
+
+export const withCustomserializer = () => {
+  const richAddon = addon;
+  const fieldOveride: FieldOverrideObject = {
+    type: 'field',
+    targets: ['text', 'title', 'button'],
+    keys: ['content'],
+    serialize: (val: string): string => {
+      const regex = /<p\s*\b/g;
+      const spanned = val
+        .replace(regex, '<span class="custom-class" ')
+        .replace(/<\/p>/g, '</span>');
+
+      return spanned;
+    },
+    unserialize: (val: string): string => {
+      const regex = /<span\s+class="custom-class"/g;
+      const spanned = val
+        .replace(regex, '<p ')
+        .replace(/<\/span>/g, '</p>');
+
+      return spanned;
+    },
+  };
+
+  return (
+    <Builder
+      addons={[baseAddon(), ckeditorFieldAddon({
+        config: {
+          licenseKey: 'GPL',
+        },
+      }), addon]}
+      value={baseContent}
+      rootBoundary={document.documentElement}
+      options={{ debug: true }}
+      onChange={action('change')}
+    />
+  );
+};
