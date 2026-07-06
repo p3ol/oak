9b8b32e2ae9f1c57a9765ee4bec1f0a304fb6fa5
Ugo Stephant <ugo@poool.fr>
2026-07-06 18:32:23 +0200

    refactor: clean code

--- packages/react/lib/DisplayableSettings/index.tsx
+++ packages/react/lib/DisplayableSettings/index.tsx
@@ -57,7 +57,7 @@ const DisplayableSettings = ({
     element,
     // Only checking on element prevents from updating the render when a sub
     // property of the element changes
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps,react-hooks/use-memo
     Object.values(element),
     component, builder, override, getSettingPriority,
   ]);
